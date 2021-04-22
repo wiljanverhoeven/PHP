ba9f88fa2e624803f79136eba2f2bbe8afd70ce4
wiljanverhoeven <wiljanverhoeven@xs4all.nl>
2021-04-22 10:19:50 +0200

    H02 af

--- H02/opdrachten-H02.php
+++ H02/opdrachten-H02.php
@@ -1,7 +1,31 @@
 <?php
 
-$a = "ik heb niet gefaald";
-$b = "die niet werken";
+$a = "ik heb niet gefaald.";
+$b = "die niet werken.";
 $c = 10000;
 
 echo $a." ik heb alleen ".$c." manieren gevonden ".$b;
+
+echo "<br>";
+
+$d ="die geen fout";
+$e ="en";
+$f ="niet";
+
+echo "mensen ".$d.$e."maken. "." werken ".$f.".";
+
+echo "<br>";
+
+$g ="ry";
+$h =", but";
+$i ="fail";
+
+echo "T".$g." and ".$i.$h." never ".$i." to "."T".$g;
+
+echo "<br>";
+
+$j ="\"omgaan";
+$k ="met";
+$c ="teleu";
+
+echo "De cursus ".$j." ".$k." ".$c."rstellingen\""." kan vanavond helaas niet doorgaan.";
